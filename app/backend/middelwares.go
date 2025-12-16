bdf41ee3f17a187396f0d1a419feeb48bb8d02fa
PiccioHub <picciohub@gmail.com>
2025-12-16 19:10:57 +0100

    Change the CORS

--- app/backend/middelwares.go
+++ app/backend/middelwares.go
@@ -34,7 +34,10 @@ func (app *Application) Logger(next http.Handler) http.Handler {
 
 func (app *Application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://192.168.1.4:5173")
+		origin := r.Header.Get("Origin")
+		if origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
